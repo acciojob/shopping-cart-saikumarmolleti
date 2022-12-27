c801cadb0b1421c8a71c696d03c31f402680a0c7
Saikumar Molleti <80804527+saikumarmolleti@users.noreply.github.com>
2022-12-27 20:55:06 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 const request = require("supertest");
-const app = require("../app.js");
-const addData = require("../js/list.js");
+const app = require("./app.js");
+const addData = require("./list.js");
 
 const generaltest = async() => {
   const res = await request(app).get("/");
